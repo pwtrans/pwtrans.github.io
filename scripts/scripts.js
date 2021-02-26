872d8abb1283a39406193342eabe404f8bb50c67
pwtrans <79248369+pwtrans@users.noreply.github.com>
2021-02-26 22:08:41 +0800

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,23 @@
+function b(){
+    let a = document.querySelector("input").value;
+    switch(a){
+        case "耗子尾汁":
+            window.location.href="words/耗子尾汁/index.html";
+            break;
+        case "黑人抬棺":
+            window.location.href="words/黑人抬棺/index.html";
+            break;
+        case "坎油揣":
+            window.location.href="words/坎油揣/index.html";
+            break;
+        case "迷hotel":
+            window.location.href="words/迷hotel/index.html";
+            break;
+        case "闹太套":
+            window.location.href="words/闹太套/index.html";
+            break;
+        case "夏啵":
+            window.location.href="words/夏啵/index.html";
+            break;
+    }
+}
