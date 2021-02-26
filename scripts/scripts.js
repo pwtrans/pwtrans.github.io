b89c9a2215260c2800d13de30213da8bf1d8d9d1
pwtrans <79248369+pwtrans@users.noreply.github.com>
2021-02-26 20:02:00 +0800

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+function b(){
+    let a = document.querySelector("input").value;
+    if(a==="耗子尾汁"||a==="耗"||a==="子"||a==="尾"||a==="汁"||a==="耗子"||a==="子尾"||a==="尾汁"||a==="耗子尾"||a==="子尾汁"){
+        window.location.href="耗子尾汁/index.html"
+    }
+}
