32db50ced9d9cffb09e31b72ce97758bb7b92003
leosu <smh2018@sina.com>
2021-03-17 20:44:08 +0800

    edit js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,6 +19,8 @@ function b(){
         case "夏啵":
             window.location.href="words/夏啵/index.html";
             break;
+        case "":
+            break;
         default:
             window.location.href="https://www.baidu.com/s?wd="+a;
 
